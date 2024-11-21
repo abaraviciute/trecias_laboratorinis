7a7ee015cfdea321ba0c4a77e6cb9a985b0923e4
Dominyka Abaravičiūtė <dominyka.abaraviciute@mif.stud.vu.lt>
2024-11-21 12:03:52 +0200

    Struktūros pakeitimas klase ir funckijų pritaikymas.

--- Stud.h
+++ Stud.h
@@ -1,21 +1,49 @@
 #pragma once
 #include "Mylib.h"
 
-struct Studentas {
-    string vardas;
-    string pavarde;
-    vector<int> nd;
-    int egzaminas;
+class Studentas {
+private:
+    string vardas_;
+    string pavarde_;
+    vector<int> nd_;
+    int egzaminas_;
+public:
     double galutinisPazymys;
     double galutinisPazymysVid;
     double galutinisPazymysMed;
+
+    inline string vardas() const { return vardas_; }
+    inline string pavarde() const { return pavarde_; }
+    inline vector<int> nd() const { return nd_; }
+    inline int egzaminas() const { return egzaminas_; }
+
+    inline void setVardas(const string& vardas) { vardas_ = vardas; }
+    inline void setPavarde(const string& pavarde) { pavarde_ = pavarde; }
+    inline void setNd(const vector<int>& nd) { nd_ = nd; }
+    inline void setEgzaminas(int egzaminas) { egzaminas_ = egzaminas; }
+
+    Studentas()
+        : vardas_(""), pavarde_(""), egzaminas_(0),
+        galutinisPazymys(0), galutinisPazymysVid(0), galutinisPazymysMed(0) {}
+
+    Studentas(const string& vardas, const string& pavarde, const vector<int>& nd, int egzaminas)
+        : vardas_(vardas), pavarde_(pavarde), nd_(nd), egzaminas_(egzaminas) {}
+
+    // Destructor
+    ~Studentas() {}
+
+    void ivestis(bool generavimas);
+    friend void isvestis(const Studentas& Lok, int ivestiesPasirinkimas);
+    double rezultatai(const string& pasirinkimas);
+    friend bool rusiavimas(const Studentas & pavarde1, const Studentas & pavarde2);
+
 };
 
 extern int rikiavimoSalyga;
 
-void ivestis(Studentas& Lok, bool generavimas);
-void isvestis(const Studentas& Lok, int ivestiesPasirinkimas);
-double rezultatai(const Studentas& Lok, const string& pasirinkimas);
+//void ivestis(Studentas& Lok, bool generavimas);
+//void isvestis(const Studentas& Lok, int ivestiesPasirinkimas);
+//double rezultatai(const Studentas& Lok, const string& pasirinkimas);
 template <typename Struktura>
 int ivestisIsFailo(const string& failas, Struktura& struktura);
 bool rusiavimas(const Studentas& pavarde1, const Studentas& pavarde2);
